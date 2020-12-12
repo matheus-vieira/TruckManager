35be6a184069469b161997e3b472a4d5f87024ed
Matheus Costa Vieira <matheus.costa.vieira@gmail.com>
2020-12-12 09:37:05 -0300

    Create a ASP.NET Core 5.0 web project

---
+++ src/TruckManager/TruckManager.WebUi/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace TruckManager.WebUi.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
